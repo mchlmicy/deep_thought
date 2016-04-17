f94bd8b6241ab17a0d7a9d36e0de77087dfad861
Michael Young <mike@bitready.io>
2016-04-16 20:37:36 -0400

    Fix

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ import sys
 
 if len(sys.argv) >= 2:
     submission, text = sys.argv[1:], ''
-    for statement in submission:
-        text += ' ' + str(statement)
+    for index, statement in enumerate(submission):
+        if index > 0:
+            text += ' ' 
+        text += str(statement)
     print classifier.predict([observe(text)])
 else:
     print 'Please submit a valid question / statement.'
